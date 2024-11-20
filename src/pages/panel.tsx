ab3c8983a067c3e434d53bbc13311eb6ef5f8e6e
ETCasual <casualorient@gmail.com>
2024-11-21 05:18:16 +0800

    update song list

--- src/pages/panel.tsx
+++ src/pages/panel.tsx
@@ -7,7 +7,7 @@ const Panel = () => {
   const dbRef = doc(db, "audio/settings");
   const { status, data } = useFirestoreDocData(dbRef);
 
-  const update = async (ans: boolean) => {
+  const update = async (ans: string) => {
     await updateDoc(dbRef, {
       play: ans,
     });
@@ -17,9 +17,7 @@ const Panel = () => {
     status === "loading"
       ? "loading"
       : status === "success"
-        ? data.play
-          ? "play"
-          : "pause"
+        ? (data.play as string)
         : "Error";
 
   return (
@@ -29,16 +27,28 @@ const Panel = () => {
           Play audio? (current: {display})
         </p>
         <button
-          onClick={() => update(true)}
+          onClick={() => update("GREATEST")}
           className={`w-full rounded-md border border-green-400 bg-green-400 py-2`}
         >
-          Play
+          GREATEST
         </button>
         <button
-          onClick={() => update(false)}
+          onClick={() => update("TIMER")}
           className={`w-full rounded-md border border-red-400 bg-green-400 py-2`}
         >
-          Stop
+          TIMER
+        </button>
+        <button
+          onClick={() => update("SQUID")}
+          className={`w-full rounded-md border border-red-400 bg-green-400 py-2`}
+        >
+          SQUID
+        </button>
+        <button
+          onClick={() => update("STOP")}
+          className={`w-full rounded-md border border-red-400 bg-green-400 py-2`}
+        >
+          STOP
         </button>
       </div>
     </main>
